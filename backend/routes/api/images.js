d68576ca44cd7d634e50cf3a14e9db4223a362da
Melody Yoo <melodyyoo72@gmail.com>
2023-05-18 15:35:00 -0700

    images complete

---
+++ backend/routes/api/images.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+
+const { Spot, SpotImage, User, Review, ReviewImage, Booking } = require("../../db/models");
+const { requireAuth } = require("../../utils/auth");
+
+router.delete("/spot-images/:imageId", requireAuth, async (req, res, next) => {
+  const image = await SpotImage.findByPk(req.params.imageId, {
+    include: { model: Spot },
+  });
+
+  if (!image) return res.status(404).json({ message: "Review Image couldn't be found" });
+
+  if (image.Spot.ownerId !== req.user.id) return res.status(403).json({ message: "Forbidden" });
+
+  await image.destroy();
+
+  res.json({ message: "Successfully deleted" });
+});
+
+router.delete("/review-images/:imageId", requireAuth, async (req, res, next) => {
+    const review = await ReviewImage.findByPk(req.params.imageId, {
+        include: {model: Review}
+    });
+
+    if(!review) return res.status(404).json({message: "Review Image couldn't be found"});
+
+    if(review.Review.userId !== req.user.id) return res.status(403).json({message: "Forbidden"});
+
+    await review.destroy();
+
+    res.json({message: "Successfully deleted"})
+
+});
+
+module.exports = router;
