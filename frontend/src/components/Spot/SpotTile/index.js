4dad9958ab6924e8f1b5494130096fadcbc772a8
Melody Yoo <melodyyoo72@gmail.com>
2023-06-06 08:31:52 -0700

    manage spots functionality

--- frontend/src/components/Spot/SpotTile/index.js
+++ frontend/src/components/Spot/SpotTile/index.js
@@ -1,27 +1,42 @@
-
 import "./SpotTile.css";
 import { useHistory } from "react-router-dom";
 
-const SpotTile = ({previewImage, location, price, avgRating, id}) =>{
-    const image = (previewImage === 'No preview image available') ?
-    <div style={{width:250, height:200, border:'solid black 1px', display:'flex', justifyContent:'center', alignItems:'center'}}>
+const SpotTile = ({ previewImage, location, price, avgRating, id }) => {
+  const image =
+    previewImage === "No preview image available" ? (
+      <div
+        style={{
+          width: 250,
+          height: 200,
+          border: "solid black 1px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         {previewImage}
-    </div> : <img src={previewImage} alt='Spot Image'/>
+      </div>
+    ) : (
+      <img src={previewImage} alt="Spot Image" />
+    );
 
-    const history = useHistory();
-    
-    return(
-        <>
-        <div className="spot-tile" onClick={(e) => history.push(`/spots/${id}`)}>
-            {image}
-            <div className="location-and-rating">
-                <p>{location}</p>
-                <p>{avgRating}</p>
-            </div>
-            <p>${price} per day</p>
+  const history = useHistory();
+
+  return (
+    <>
+      <div className="spot-tile" onClick={(e) => history.push(`/spots/${id}`)}>
+        {image}
+        <div className="location-and-rating">
+          <p>{location}</p>
+          <div style={{display:'flex'}}>
+            <i className="fa-solid fa-star" style={{fontWeight: 150, fontSize: 12}}></i>
+            <p>{avgRating}</p>
+          </div>
         </div>
-        </>
-    )
-}
+        <p>${price} per day</p>
+      </div>
+    </>
+  );
+};
 
 export default SpotTile;
